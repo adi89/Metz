df571fa4e752e1a2f7cdb2d5ff44a44b47b1c1cf
Adi Singh <adityasingh89@gmail.com>
2013-12-05 22:52:25 -0500

    methods should follow srp too

--- chapter2/srp1.rb
+++ chapter2/srp1.rb
@@ -1,24 +1,31 @@
 # we don't ever want to depend on structures of arrays, so that we'd need to know it's index to accomplish anything. if we're using an array, we gotta define it in one place, so that any change in the structure would only be affected where it's declared.
 
+require 'pry-debugger'
+
 class BJJ
   attr_accessor :belts
   def initialize(data)
     @belts = gracify(data)
   end
 
-  Belt = Struct.new(:training_days, :competitions) #structs are a cool way to plug in your data. this lets you call Struct.attribute, as opposed to a sepcific index.
+  Belt = Struct.new(:training_days, :competitions)
+  #structs are a cool way to plug in your data. this lets you call Struct.attribute, as opposed to a sepcific index.
   def gracify(data)
     data.map{|i| Belt.new(i[0], i[1])}
   end
 
-  def rankScore
-    belts.map{|i| i.training_days + (20 * i.competitions)}
+  def rank_score(training_days, competitions)
+    training_days + 20* competitions
+  end
+
+  def rank_scores
+    belts.map{|i| rank_score(i.training_days, (i.competitions))}
   end
 
 end
 
 data = [[230, 34],[450, 12]]
 #we thus see that these indexes are only taken into account in the gracify method ---  encapsulation.
-score = BJJ.new(data).rankScore
+jiu_jitsu = BJJ.new(data)
 
-puts "#{score}"
+puts "#{jiu_jitsu.rank_scores}"
